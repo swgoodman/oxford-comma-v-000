36d8eca408a7938a702462c918c3fdad8a274818
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-24 21:17:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array.join(" and ")}"
+  else array.length > 2
+    return "#{array[0, array.length-1].join(", ")}, and #{array.last}"
+  end
 end
