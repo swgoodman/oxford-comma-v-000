33663725d68301245df963600682f87ad26597a0
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-24 21:14:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
-  if array.length > 3
-    return array.join (" and ")
+  if array.length == 1
+    return array[0]]
   else array.length > 3
     return "#{array[0, array.length].join(", ")} and #{array.last}"
   end
