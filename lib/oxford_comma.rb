085f4a3a356ecbf011202ba063ea8e24f0ea852c
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-24 21:16:48 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 1
     return array[0]
-  else array.length >= 2
+  else array.length > 2
     return "#{array[0, array.length-1].join(", ")}, and #{array.last}"
   end
 end
